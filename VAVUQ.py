ccb01eb3806798e9815212b1cf553569df92d242
vavuq <github@jecourtney.com>
2016-07-05 16:31:52 -0700

    Update VAVUQ.py

--- VAVUQ.py
+++ VAVUQ.py
@@ -211,7 +211,7 @@ def inter_fun(x,y,z,XI,YI,interp_method):
     if interp_method == 'Spline':
         from scipy.interpolate import RectBivariateSpline
         x, y = np.sort(np.unique(x)), np.sort(np.unique(y))
-        spl = RectBivariateSpline(x,y,z.H)
+        spl = RectBivariateSpline(x,y,z)
         imat = spl.ev(XI, YI)
     elif interp_method == 'Cubic':
         from scipy.interpolate import griddata
